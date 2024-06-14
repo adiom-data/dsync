d57c615d3a0d70d7e6a0f159afbe7651557c3482
Alexander Komyagin <adkomyagin@gmail.com>
2024-06-13 20:03:50 -0700

    Test for connector writes

--- protocol/iface/test/connector_testsuite.go
+++ protocol/iface/test/connector_testsuite.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 /**
@@ -36,6 +37,7 @@ func (suite *ConnectorTestSuite) SetupSuite() {
 func (suite *ConnectorTestSuite) TearDownSuite() {
 }
 
+// Check that the connector can read
 func (suite *ConnectorTestSuite) TestConnectorReadAll() {
 	ctx := context.Background()
 
@@ -46,7 +48,7 @@ func (suite *ConnectorTestSuite) TestConnectorReadAll() {
 	// transport should return the mock coordinator endpoint
 	t.On("GetCoordinatorEndpoint", mock.Anything).Return(c, nil)
 	// coordinator should return a connector ID on registration
-	testConnectorID := iface.ConnectorID{ID: "1234"}
+	testConnectorID := iface.ConnectorID{ID: "1"}
 	var caps iface.ConnectorCapabilities
 	c.On("RegisterConnector", mock.Anything, mock.Anything).Return(testConnectorID, nil).Run(func(args mock.Arguments) {
 		// Store advertised connector capabilities to skip irrelevant tests
@@ -111,13 +113,106 @@ func (suite *ConnectorTestSuite) TestConnectorReadAll() {
 	time.Sleep(1 * time.Second)
 
 	// We should have gotten some data in the channel
-	assert.Greater(suite.T(), messageCount, 0)
+	assert.True(suite.T(), messageCount > 0, "Should have read some data")
 	// A notification should have been sent to the coordinator that the job is done
 	c.AssertCalled(suite.T(), "NotifyDone", flowID, testConnectorID)
 
 	connector.Teardown()
 }
 
+// Check that the connector can write
 func (suite *ConnectorTestSuite) TestConnectorWrite() {
+	ctx := context.Background()
 
+	// create mocks for the transport and coordinator
+	t := new(mocks.Transport)
+	c := new(mocks.Coordinator)
+
+	// transport should return the mock coordinator endpoint
+	t.On("GetCoordinatorEndpoint", mock.Anything).Return(c, nil)
+	// coordinator should return a connector ID on registration
+	testConnectorID := iface.ConnectorID{ID: "2"}
+	var caps iface.ConnectorCapabilities
+	c.On("RegisterConnector", mock.Anything, mock.Anything).Return(testConnectorID, nil).Run(func(args mock.Arguments) {
+		// Store advertised connector capabilities to skip irrelevant tests
+		caps = args.Get(0).(iface.ConnectorDetails).Cap // Perform a type assertion here
+	})
+
+	// create a new connector object
+	connector := suite.connectorFactoryFunc()
+
+	// setup the connector and make sure it returns no errors
+	err := connector.Setup(ctx, t)
+	assert.NoError(suite.T(), err)
+
+	// check that the mocked methods were called
+	t.AssertExpectations(suite.T())
+	c.AssertExpectations(suite.T())
+
+	// Check if the connector supports sink capabilities
+	if !caps.Sink {
+		suite.T().Skip("Skipping test because this connector does not support sink capabilities")
+	}
+
+	// Do some prep
+	flowID := iface.FlowID{ID: "2234"}
+	dataChannelID := iface.DataChannelID{ID: "4321"}
+	dataChannel := make(chan iface.DataMessage)
+	defer close(dataChannel)
+
+	t.On("GetDataChannelEndpoint", dataChannelID).Return(dataChannel, nil)
+	c.On("NotifyDone", flowID, testConnectorID).Return(nil)
+	messageIterCount := 1000
+
+	// Start a go routine to write to the data channel
+	go func() {
+		loc := iface.Location{Database: "test", Collection: "testX"}
+		lsn := int64(0)
+		// write a number of messages to the channel
+		for i := 0; i < messageIterCount; i++ {
+			//do a simple 'pre-fix' random message sequence
+			id := i
+			doc := bson.M{"_id": id, "test": i}
+			updatedDoc := bson.M{"_id": id, "test": i + 1}
+
+			idType, idVal, _ := bson.MarshalValue(id)
+			bsonDataRaw, _ := bson.Marshal(doc)
+			bsonDataRawUpdated, _ := bson.Marshal(updatedDoc)
+
+			dataChannel <- iface.DataMessage{Data: &bsonDataRaw, MutationType: iface.MutationType_Insert, Loc: loc, SeqNum: lsn}
+			lsn++
+			dataChannel <- iface.DataMessage{Data: &bsonDataRawUpdated, MutationType: iface.MutationType_Update, Loc: loc, SeqNum: lsn, Id: &idVal, IdType: byte(idType)}
+			lsn++
+			dataChannel <- iface.DataMessage{MutationType: iface.MutationType_Delete, Loc: loc, SeqNum: lsn, Id: &idVal, IdType: byte(idType)}
+			lsn++
+		}
+	}()
+
+	// Test writing to the sink
+	// We'll run this with a timeout to make sure it's non-blocking
+	err = RunWithTimeout(suite.T(), connector, func(receiver interface{}, args ...interface{}) error {
+		return receiver.(iface.Connector).StartWriteFromChannel(args[0].(iface.FlowID), args[1].(iface.DataChannelID))
+	}, NonBlockingTimeout,
+		flowID, dataChannelID)
+	assert.NoError(suite.T(), err)
+
+	// Sleep for a bit
+	suite.T().Log("Sleeping for 10 seconds to allow the connector to write data")
+	time.Sleep(10 * time.Second)
+
+	// The connector should still be running right now and writing data from the channel - let's interrupt it
+	err = RunWithTimeout(suite.T(), connector, func(receiver interface{}, args ...interface{}) error {
+		return receiver.(iface.ConnectorICoordinatorSignal).Interrupt(args[0].(iface.FlowID))
+	}, NonBlockingTimeout, flowID)
+	assert.NoError(suite.T(), err)
+
+	// Sleep for 1 second to ensure that the interruption took effect
+	time.Sleep(1 * time.Second)
+
+	// We should have written some data from the channel, so the LSN should be greater than 0
+	assert.True(suite.T(), connector.GetConnectorStatus(flowID).WriteLSN > 0, "Should have written some data")
+	// A notification should have been sent to the coordinator that the job is done
+	c.AssertCalled(suite.T(), "NotifyDone", flowID, testConnectorID)
+
+	connector.Teardown()
 }
