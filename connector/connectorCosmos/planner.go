3540334fc613dcc761e7e295d7a014367f981c32
Simone Kudia <simonekudia04@gmail.com>
2024-07-22 15:37:40 -0700

    progress on concurrent change streams

--- connector/connectorCosmos/planner.go
+++ connector/connectorCosmos/planner.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/adiom-data/dsync/protocol/iface"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
@@ -109,21 +108,4 @@ func (cc *CosmosConnector) getAllCollections(dbName string) ([]string, error) {
 	})
 
 	return collections, nil
-}
-
-// creates a filter for the change stream to include only the specified namespaces
-func createChangeStreamNamespaceFilterFromTasks(tasks []iface.ReadPlanTask) bson.D {
-	var filters []bson.D
-	for _, task := range tasks {
-		filters = append(filters, bson.D{{"ns.db", task.Def.Db}, {"ns.coll", task.Def.Col}})
-	}
-	return bson.D{{"$or", filters}}
-}
-
-// create a change stream filter that covers all namespaces except system
-func createChangeStreamNamespaceFilter() bson.D {
-	return bson.D{{"$and", []bson.D{
-		{{"ns.db", bson.D{{"$regex", primitive.Regex{Pattern: ExcludedDBPatternCS}}}}},
-		{{"ns.coll", bson.D{{"$regex", primitive.Regex{Pattern: ExcludedSystemCollPatternCS}}}}},
-	}}}
 }
