60b1d2070c8c088cc55c3808405d924b485ef459
Alex Komyagin <31981287+alex-thc@users.noreply.github.com>
2024-08-06 10:34:10 -0700

    Cosmos Partitioning (#23)
    Cosmos DB connector support for partitioned tasks.
    
    * Automatic partitioning based on ObjectId _id values for large collections
    * Documentation update

--- connector/connectorCosmos/planner.go
+++ connector/connectorCosmos/planner.go
@@ -7,35 +7,36 @@
 package connectorCosmos
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"regexp"
 	"slices"
 	"strings"
+	"sync"
 
 	"github.com/adiom-data/dsync/protocol/iface"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 var (
-	MaxNumTasks = 8
 	// System databases that we don't want to copy
 	ExcludedDBListForIC = []string{"local", "config", "admin", "adiom-internal"}
 	// System collections that we don't want to copy (regex pattern)
 	ExcludedSystemCollPattern = "^system[.]"
 )
 
-func (cc *CosmosConnector) createInitialCopyTasks(namespaces []string) ([]iface.ReadPlanTask, error) {
+// returns the list of namespaces and the tasks to be executed (partitioned if necessary)
+func (cc *CosmosConnector) createInitialCopyTasks(namespaces []string) ([]namespace, []iface.ReadPlanTask, error) {
 	var dbsToResolve []string //database names that we need to resolve
 
-	var tasks []iface.ReadPlanTask
-	taskId := iface.ReadPlanTaskID(1)
+	var nsTasks []namespace
 
 	if namespaces == nil {
 		var err error
 		dbsToResolve, err = cc.getAllDatabases()
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 	} else {
 		// iterate over provided namespaces
@@ -44,12 +45,7 @@ func (cc *CosmosConnector) createInitialCopyTasks(namespaces []string) ([]iface.
 		for _, ns := range namespaces {
 			db, col, isFQN := strings.Cut(ns, ".")
 			if isFQN {
-				task := iface.ReadPlanTask{Id: taskId}
-				task.Def.Db = db
-				task.Def.Col = col
-
-				tasks = append(tasks, task)
-				taskId++
+				nsTasks = append(nsTasks, namespace{db, col})
 			} else {
 				dbsToResolve = append(dbsToResolve, ns)
 			}
@@ -62,23 +58,179 @@ func (cc *CosmosConnector) createInitialCopyTasks(namespaces []string) ([]iface.
 	for _, db := range dbsToResolve {
 		colls, err := cc.getAllCollections(db)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		//create tasks for these
 		for _, coll := range colls {
-			task := iface.ReadPlanTask{Id: taskId}
-			task.Def.Db = db
-			task.Def.Col = coll
-
-			tasks = append(tasks, task)
-			taskId++
+			nsTasks = append(nsTasks, namespace{db, coll})
 		}
 	}
 
-	if len(tasks) > MaxNumTasks {
-		return nil, fmt.Errorf("too many tasks to copy: %d, max %d", len(tasks), MaxNumTasks)
+	if len(nsTasks) > cc.settings.maxNumNamespaces {
+		return nil, nil, fmt.Errorf("too many namespaces to copy: %d, max %d", len(nsTasks), cc.settings.maxNumNamespaces)
 	}
-	return tasks, nil
+
+	partitionedTasks, err := cc.partitionTasksIfNecessary(nsTasks)
+	return nsTasks, partitionedTasks, err
+}
+
+// partitionTasksIfNecessary checks all the namespace tasks and partitions them if necessary
+// returns the final list of tasks to be executed with unique task ids
+func (cc *CosmosConnector) partitionTasksIfNecessary(namespaceTasks []namespace) ([]iface.ReadPlanTask, error) {
+	countCheckChannel := make(chan namespace, len(namespaceTasks))
+	approxTasksChannel := make(chan iface.ReadPlanTask, len(namespaceTasks))
+
+	finalTasksChannel := make(chan iface.ReadPlanTask, len(namespaceTasks))
+	finalTasks := make([]iface.ReadPlanTask, 0, len(namespaceTasks))
+
+	// add namespace tasks to the queue to check their counts
+	go func() {
+		for _, task := range namespaceTasks {
+			countCheckChannel <- task
+		}
+		close(countCheckChannel)
+	}()
+
+	// do parallel counting (async) and generate approximate boundaries
+	go cc.parallelNamespaceTaskPreparer(countCheckChannel, finalTasksChannel, approxTasksChannel)
+	// do parallel task generation based on approximate boundaries (skip those with matching boundaries - empty tasks)
+	go cc.parallelTaskBoundsClarifier(approxTasksChannel, finalTasksChannel)
+
+	// get all the finalized tasks and assign sequential task ids
+	taskId := iface.ReadPlanTaskID(1)
+	for task := range finalTasksChannel {
+		task.Id = taskId
+		finalTasks = append(finalTasks, task)
+		taskId++
+	}
+	return finalTasks, nil
+}
+
+// In parallel clarifies bounds for tasks that have approximate boundaries
+// Replaces the approximate bound with the closest lower or equal value found
+// Disconiues the task if the bounds are the same
+// Outputs finalized tasks into the finalTasksChannel
+func (cc *CosmosConnector) parallelTaskBoundsClarifier(approxTasksChannel <-chan iface.ReadPlanTask, finalTasksChannel chan<- iface.ReadPlanTask) {
+	//define workgroup
+	wg := sync.WaitGroup{}
+	wg.Add(cc.settings.numParallelPartitionWorkers)
+	// create workers to do the work
+	for i := 0; i < cc.settings.numParallelPartitionWorkers; i++ {
+		go func() {
+			for task := range approxTasksChannel {
+				collection := cc.client.Database(task.Def.Db).Collection(task.Def.Col)
+
+				// first, clarify the low value
+				valLow, errLow := findClosestLowerValue(cc.ctx, collection, task.Def.PartitionKey, task.Def.Low)
+				if errLow != nil {
+					slog.Error(fmt.Sprintf("Failed to clarify low value for task %v: %v", task, errLow))
+					continue //XXX: this task will be skipped, should we panic here?
+				}
+
+				// then, clarify the high value
+				valHigh, errHigh := findClosestLowerValue(cc.ctx, collection, task.Def.PartitionKey, task.Def.High)
+				if errHigh != nil {
+					slog.Error(fmt.Sprintf("Failed to clarify high value for task %v: %v", task, errHigh))
+					continue //XXX: this task will be skipped, should we panic here?
+				}
+
+				// if the bounds are the same, then we don't need to do anything
+				if !valLow.Equal(valHigh) {
+					task.Def.Low = valLow
+					task.Def.High = valHigh
+					finalTasksChannel <- task
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	// wait for all workers to finish and close the finalTasksChannel channel
+	wg.Wait()
+	close(finalTasksChannel)
+}
+
+func createReadPlanTaskForNs(ns namespace) iface.ReadPlanTask {
+	task := iface.ReadPlanTask{}
+	task.Def.Db = ns.db
+	task.Def.Col = ns.col
+	return task
+}
+
+// In parallel checks the count of the namespace and segregates them into two channels
+// One for finalized tasks, and another for partitioned tasks with approximate bounds that need to be clarified
+func (cc *CosmosConnector) parallelNamespaceTaskPreparer(countCheckChannel <-chan namespace, finalTasksChannel chan<- iface.ReadPlanTask, approxTasksChannel chan<- iface.ReadPlanTask) {
+	//define workgroup
+	wg := sync.WaitGroup{}
+	wg.Add(cc.settings.numParallelPartitionWorkers)
+	// create workers to do the counting
+	for i := 0; i < cc.settings.numParallelPartitionWorkers; i++ {
+		go func() {
+			for nsTask := range countCheckChannel {
+				collection := cc.client.Database(nsTask.db).Collection(nsTask.col)
+				count, err := collection.EstimatedDocumentCount(cc.ctx)
+				if err != nil {
+					if cc.ctx.Err() == context.Canceled {
+						slog.Debug(fmt.Sprintf("Count error: %v, but the context was cancelled", err))
+					} else {
+						slog.Warn(fmt.Sprintf("Failed to count documents, not splitting: %v", err))
+						finalTasksChannel <- createReadPlanTaskForNs(nsTask)
+						continue
+					}
+				}
+
+				if count < cc.settings.targetDocCountPerPartition*2 { //not worth doing anything
+					finalTasksChannel <- createReadPlanTaskForNs(nsTask)
+				} else { //we need to split it
+					slog.Debug(fmt.Sprintf("Need to split task %v with count %v", nsTask, count))
+					//get min and max bounds
+					min, max, err := cc.getMinAndMax(cc.ctx, nsTask, cc.settings.partitionKey)
+					if err != nil {
+						slog.Warn(fmt.Sprintf("Failed to get min and max boundaries for a task %v so not splitting: %v", nsTask, err))
+						finalTasksChannel <- createReadPlanTaskForNs(nsTask)
+						continue
+					}
+					slog.Debug(fmt.Sprintf("Min and max boundaries for task %v: %v, %v", nsTask, min, max))
+					// find approximate split points
+					numParts := int(count / cc.settings.targetDocCountPerPartition)
+					approxBounds, err := splitRange(min, max, numParts)
+					if err != nil {
+						slog.Warn(fmt.Sprintf("Failed to split range for task %v so not splitting: %v", nsTask, err))
+						finalTasksChannel <- createReadPlanTaskForNs(nsTask)
+						continue
+					}
+					slog.Debug(fmt.Sprintf("Number of approximate split points for task %v: %v", nsTask, len(approxBounds)))
+
+					//send min and max tasks to finalized channel
+					minTask := createReadPlanTaskForNs(nsTask)
+					minTask.Def.PartitionKey = cc.settings.partitionKey
+					minTask.Def.High = min //only setting the high boundary indicating that we want (-INF, min)
+
+					maxTask := createReadPlanTaskForNs(nsTask)
+					maxTask.Def.PartitionKey = cc.settings.partitionKey
+					maxTask.Def.Low = max //only setting the low boundary indicating that we want (max, INF)
+
+					finalTasksChannel <- minTask
+					finalTasksChannel <- maxTask
+
+					//send the tasks with approx bounaries downstream
+					for i := 0; i < len(approxBounds)-1; i++ {
+						task := createReadPlanTaskForNs(nsTask)
+						task.Def.PartitionKey = cc.settings.partitionKey
+						task.Def.Low = approxBounds[i]
+						task.Def.High = approxBounds[i+1]
+						approxTasksChannel <- task
+					}
+
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	// wait for all workers to finish and close the approxTasksChannel channel
+	wg.Wait()
+	close(approxTasksChannel)
 }
 
 // get all database names except system databases
---
+++ docs-dev/TasksPartitioning.md
@@ -0,0 +1,9 @@
+# Tasks and Partitioning
+
+To allow for parallel execution of reads and more granular resumability, we implemented an abstraction called Tasks. They correspond to database namespaces (collections). A task can cover the whole namespace, or a part of it, if the specific bounds and partition key are provided.
+
+For collections with a lot of documents (e.g. 50 mil+), it's best to split the task into parts. This way the parts can be executed in parallel and much less progress will be lost in case the process needs to be stopped and resumed later.
+
+Unfortunately, Cosmos DB with MongoDB API doesn't properly support $sample in the aggregation framework or any other methods for statistical splitting that doesn't require scanning the whole collection. For Cosmos DB sources, we implemented a partitioning strategy based on the actual _id values, taking advantage of the Law Of Big Numbers. When _id is of ObjectId type which is a timestamp-based format, and the number of documents is in 10's of millions or more, we create partitions by splitting the time range in the collection into equal parts. This works well for large collections with relatively consisent workloads.
+
+The target partition size is set to 512,000 documents and is subject to change in future versions. This specific value was chosen to provide a meaningful number of partitions without creating much unnecceary overhead.
