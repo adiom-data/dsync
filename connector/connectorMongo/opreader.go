fc5e0ab9edcf075c0fecd461c2813444ef743b28
Alex Komyagin <31981287+alex-thc@users.noreply.github.com>
2024-08-08 13:18:09 -0700

    Fix writes in MongoConnector (#24)
    Parallelized
    Efficient (batch insert passthrough)
    Correct (a single id can only go to a specific worker)
    Supports barriers

--- connector/connectorMongo/opreader.go
+++ connector/connectorMongo/opreader.go
@@ -38,13 +38,20 @@ func (mc *MongoConnector) convertChangeStreamEventToDataMessage(change bson.M) (
 
 	switch optype {
 	case "insert":
+		// get the id of the document that was inserted
+		id := change["documentKey"].(bson.M)["_id"]
+		// convert id to raw bson
+		idType, idVal, err := bson.MarshalValue(id)
+		if err != nil {
+			return iface.DataMessage{}, fmt.Errorf("failed to marshal _id: %v", err)
+		}
 		fullDocument := change["fullDocument"].(bson.M)
 		// convert fulldocument to BSON.Raw
 		fullDocumentRaw, err := bson.Marshal(fullDocument)
 		if err != nil {
 			return iface.DataMessage{}, fmt.Errorf("failed to marshal full document: %v", err)
 		}
-		dataMsg = iface.DataMessage{Loc: loc, Data: &fullDocumentRaw, MutationType: iface.MutationType_Insert}
+		dataMsg = iface.DataMessage{Loc: loc, Id: &idVal, IdType: byte(idType), Data: &fullDocumentRaw, MutationType: iface.MutationType_Insert}
 	case "update":
 		// get the id of the document that was changed
 		id := change["documentKey"].(bson.M)["_id"]
