3f20742c14ee230f048299f6edb493105da5ae72
Alex Komyagin <31981287+alex-thc@users.noreply.github.com>
2024-08-16 09:25:26 -0700

    Ux cleanup (#30)
    * Add check for genuine MongoDB in statestore
    
    * Make statestore and sourcetype optional

---
+++ statestore/statestoreTemp/statestoreTemp.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 Adiom, Inc.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+package statestoreTemp
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+)
+
+/**
+ * Implements the iface.Statestore interface
+ * Does exactly nothing - pure stub
+ */
+
+type TempStateStore struct {
+}
+
+func NewMongoStateStore() *TempStateStore {
+	return &TempStateStore{}
+}
+
+func (s *TempStateStore) Setup(ctx context.Context) error {
+	slog.Debug("Setting up temp state store")
+	return nil
+}
+
+func (s *TempStateStore) Teardown() {
+}
+
+func (s *TempStateStore) PersistObject(storeName string, id interface{}, obj interface{}) error {
+	return nil
+}
+
+func (s *TempStateStore) RetrieveObject(storeName string, id interface{}, obj interface{}) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *TempStateStore) DeleteObject(storeName string, id interface{}) error {
+	return nil
+}
