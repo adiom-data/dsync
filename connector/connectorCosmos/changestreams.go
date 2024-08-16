6560ce4da83c5a8a1604ad3853afbcf0fce216c9
simonekudia <123107669+simonekudia@users.noreply.github.com>
2024-08-16 09:24:47 -0700

    27-bug-in-cosmos-reader-resume (#29)
    Fixing  resume bug when change streams are created per task instead of per-namespace
    
    ---------
    
    Co-authored-by: Alexander Komyagin <adkomyagin@gmail.com>

--- connector/connectorCosmos/changestreams.go
+++ connector/connectorCosmos/changestreams.go
@@ -7,6 +7,7 @@ package connectorCosmos
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"sync"
@@ -35,19 +36,19 @@ func (cc *CosmosConnector) createChangeStream(ctx context.Context, namespace ifa
 }
 
 // Creates parallel change streams for each task in the read plan, and processes the events concurrently
-func (cc *CosmosConnector) StartConcurrentChangeStreams(ctx context.Context, tasks []iface.ReadPlanTask, readerProgress *ReaderProgress, channel chan<- iface.DataMessage) error {
+func (cc *CosmosConnector) StartConcurrentChangeStreams(ctx context.Context, namespaces []namespace, readerProgress *ReaderProgress, channel chan<- iface.DataMessage) error {
 	var wg sync.WaitGroup
 	// global atomic lsn counter
 	var lsn int64 = 0
 
 	cc.status.CDCActive = true
 	// iterate over all tasks and start a change stream for each
-	for _, task := range tasks {
+	for _, ns := range namespaces {
 		wg.Add(1)
-		go func(task iface.ReadPlanTask) {
+		go func(ns namespace) {
 			defer wg.Done()
 			//get task location and retrieve resume token
-			loc := iface.Location{Database: task.Def.Db, Collection: task.Def.Col}
+			loc := iface.Location{Database: ns.db, Collection: ns.col}
 			slog.Info(fmt.Sprintf("Connector %s is starting to read change stream for flow %s at namespace %s.%s", cc.id, cc.flowId, loc.Database, loc.Collection))
 
 			token, err := cc.flowCDCResumeTokenMap.GetToken(loc)
@@ -58,10 +59,10 @@ func (cc *CosmosConnector) StartConcurrentChangeStreams(ctx context.Context, tas
 			opts := moptions.ChangeStream().SetResumeAfter(token).SetFullDocument(moptions.UpdateLookup)
 			changeStream, err := cc.createChangeStream(ctx, loc, opts)
 			if err != nil {
-				if ctx.Err() == context.Canceled {
-					slog.Debug(fmt.Sprintf("Failed to create change stream for task %v, namespace %s.%s: %v, but the context was cancelled", task.Id, task.Def.Db, task.Def.Col, err))
+				if errors.Is(context.Canceled, ctx.Err()) {
+					slog.Debug(fmt.Sprintf("Failed to create change stream for namespace %s.%s: %v, but the context was cancelled", loc.Database, loc.Collection, err))
 				} else {
-					slog.Error(fmt.Sprintf("Failed to create change stream for task %v, namespace %s.%s: %v", task.Id, task.Def.Db, task.Def.Col, err))
+					slog.Error(fmt.Sprintf("Failed to create change stream for namespace %s.%s: %v", loc.Database, loc.Collection, err))
 				}
 				return
 			}
@@ -78,7 +79,7 @@ func (cc *CosmosConnector) StartConcurrentChangeStreams(ctx context.Context, tas
 				}
 			}
 
-		}(task)
+		}(ns)
 	}
 	wg.Wait()
 	return nil
