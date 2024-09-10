5817570a1ede72e740beca51df484f06801bf7ce
Alexander Komyagin <adkomyagin@gmail.com>
2024-09-10 14:06:56 -0700

    Better accounting

--- connectors/cosmos/util.go
+++ connectors/cosmos/util.go
@@ -219,6 +219,7 @@ func (cc *Connector) restoreProgressDetails(tasks []iface.ReadPlanTask) { //XXX:
 				TasksStarted:        0,
 				DocsCopied:          0,
 				EstimatedDocsCopied: 0,
+				ActiveTasksList:     make(map[iface.ReadPlanTaskID]bool),
 			}
 			cc.status.ProgressMetrics.NamespaceProgress[ns] = nsStatus
 		}
@@ -233,7 +234,6 @@ func (cc *Connector) restoreProgressDetails(tasks []iface.ReadPlanTask) { //XXX:
 			nsStatus.DocsCopied += task.DocsCopied
 
 			nsStatus.EstimatedDocsCopied += task.EstimatedDocCount
-			slog.Debug(fmt.Sprintf("totalDocsCopied: %v, ns docs copied: %v", cc.status.ProgressMetrics.NumDocsSynced, nsStatus.DocsCopied))
 		}
 	}
 	cc.status.ProgressMetrics.NumNamespaces = int64(len(cc.status.ProgressMetrics.NamespaceProgress))
@@ -250,21 +250,35 @@ func (cc *Connector) restoreProgressDetails(tasks []iface.ReadPlanTask) { //XXX:
 }
 
 // Updates the progress metrics once a task has been started
-func (cc *Connector) taskStartedProgressUpdate(nsStatus *iface.NamespaceStatus) {
+func (cc *Connector) taskStartedProgressUpdate(nsStatus *iface.NamespaceStatus, taskId iface.ReadPlanTaskID) {
 	cc.muProgressMetrics.Lock()
+	nsStatus.ActiveTasksList[taskId] = true
 	cc.status.ProgressMetrics.TasksStarted++
 	nsStatus.TasksStarted++
 	cc.muProgressMetrics.Unlock()
 }
 
 // Updates the progress metrics once a task has been completed
-func (cc *Connector) taskDoneProgressUpdate(nsStatus *iface.NamespaceStatus) {
+func (cc *Connector) taskDoneProgressUpdate(nsStatus *iface.NamespaceStatus, taskId iface.ReadPlanTaskID) {
 	cc.muProgressMetrics.Lock()
 	//update progress counters: num tasks completed
 	cc.status.ProgressMetrics.TasksCompleted++
 	nsStatus.TasksCompleted++
+
+	//go through all the tasks
+	// - mark ours as completed
+	// - calculate the approximate number of docs copied based on per-task estimates
+	approxDocsCopied := int64(0)
+	for i, task := range nsStatus.Tasks {
+		if task.Id == taskId {
+			nsStatus.Tasks[i].Status = iface.ReadPlanTaskStatus_Completed
+		}
+		if task.Status == iface.ReadPlanTaskStatus_Completed {
+			approxDocsCopied += task.EstimatedDocCount
+		}
+	}
 	//update the estimated docs copied count for the namespace to keep percentage proportional
-	nsStatus.EstimatedDocsCopied = int64(math.Max(float64(nsStatus.EstimatedDocsCopied), float64(nsStatus.TasksCompleted*nsStatus.Tasks[0].EstimatedDocCount)))
+	nsStatus.EstimatedDocsCopied = int64(math.Max(float64(nsStatus.EstimatedDocsCopied), float64(approxDocsCopied)))
 	//check if namespace has been completed
 	if nsStatus.TasksCompleted == int64(len(nsStatus.Tasks)) {
 		cc.status.ProgressMetrics.NumNamespacesCompleted++
@@ -272,6 +286,8 @@ func (cc *Connector) taskDoneProgressUpdate(nsStatus *iface.NamespaceStatus) {
 	//decrement the tasks started counter
 	cc.status.ProgressMetrics.TasksStarted--
 	nsStatus.TasksStarted--
+	//remove the task from the active tasks list
+	delete(nsStatus.ActiveTasksList, taskId)
 	cc.muProgressMetrics.Unlock()
 }
 
