eca409bf99196d3cbb874600c43a7332bf565c07
adiom-mark <mark@adiom.io>
2024-10-28 16:54:51 -0700

    [fix] Fix documents nested in array (#105)

--- connectors/common/hash_test.go
+++ connectors/common/hash_test.go
@@ -13,11 +13,11 @@ import (
 
 func compareHash(t *testing.T, hasher hash.Hash64, left []byte, right []byte) bool {
 	hasher.Reset()
-	assert.NoError(t, common.HashBson(hasher, left))
+	assert.NoError(t, common.HashBson(hasher, left, false))
 	l := hasher.Sum64()
 	assert.NotZero(t, l)
 	hasher.Reset()
-	assert.NoError(t, common.HashBson(hasher, right))
+	assert.NoError(t, common.HashBson(hasher, right, false))
 	r := hasher.Sum64()
 	assert.NotZero(t, r)
 	return l == r
@@ -25,15 +25,15 @@ func compareHash(t *testing.T, hasher hash.Hash64, left []byte, right []byte) bo
 
 func Test_HashBson(t *testing.T) {
 	// These are all the same, but the documents are reordered in various ways
-	b1, _ := bson.Marshal(bson.D{{"a", bson.A{"a", "b"}}, {"b", bson.D{{"a", "1"}, {"b", "2"}}}})
-	b2, _ := bson.Marshal(bson.D{{"b", bson.D{{"a", "1"}, {"b", "2"}}}, {"a", bson.A{"a", "b"}}})
-	b3, _ := bson.Marshal(bson.D{{"a", bson.A{"a", "b"}}, {"b", bson.D{{"b", "2"}, {"a", "1"}}}})
-	b4, _ := bson.Marshal(bson.D{{"b", bson.D{{"b", "2"}, {"a", "1"}}}, {"a", bson.A{"a", "b"}}})
+	b1, _ := bson.Marshal(bson.D{{"a", bson.A{"a", bson.D{{"b", "2"}, {"c", "3"}}}}, {"b", bson.D{{"a", "1"}, {"b", "2"}}}})
+	b2, _ := bson.Marshal(bson.D{{"b", bson.D{{"a", "1"}, {"b", "2"}}}, {"a", bson.A{"a", bson.D{{"b", "2"}, {"c", "3"}}}}})
+	b3, _ := bson.Marshal(bson.D{{"a", bson.A{"a", bson.D{{"c", "3"}, {"b", "2"}}}}, {"b", bson.D{{"b", "2"}, {"a", "1"}}}})
+	b4, _ := bson.Marshal(bson.D{{"b", bson.D{{"b", "2"}, {"a", "1"}}}, {"a", bson.A{"a", bson.D{{"b", "2"}, {"c", "3"}}}}})
 	same1 := [][]byte{b1, b2, b3, b4}
 
 	// The bson.A is reordered here
-	b5, _ := bson.Marshal(bson.D{{"a", bson.A{"b", "a"}}, {"b", bson.D{{"a", "1"}, {"b", "2"}}}})
-	b6, _ := bson.Marshal(bson.D{{"b", bson.D{{"a", "1"}, {"b", "2"}}}, {"a", bson.A{"b", "a"}}})
+	b5, _ := bson.Marshal(bson.D{{"a", bson.A{bson.D{{"b", "2"}, {"c", "3"}}, "a"}}, {"b", bson.D{{"a", "1"}, {"b", "2"}}}})
+	b6, _ := bson.Marshal(bson.D{{"b", bson.D{{"a", "1"}, {"b", "2"}}}, {"a", bson.A{bson.D{{"c", "3"}, {"b", "2"}}, "a"}}})
 	same2 := [][]byte{b5, b6}
 
 	hasher := xxhash.New()
@@ -76,5 +76,5 @@ func Test_HashBson(t *testing.T) {
 	}
 
 	// Sanity check an error case
-	assert.Error(t, common.HashBson(hasher, []byte{1, 2}))
+	assert.Error(t, common.HashBson(hasher, []byte{1, 2}, false))
 }
