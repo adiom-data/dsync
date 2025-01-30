d1203a023711f80aed4fd438d154f4054c3f9eb5
adiom-mark <mark@adiom.io>
2025-01-30 09:51:45 -0800

    export some utils (#191)

---
+++ pkg/app/options/options.go
@@ -0,0 +1,14 @@
+package options
+
+import "github.com/adiom-data/dsync/internal/app/options"
+
+var ErrMissingConnector = options.ErrMissingConnector
+var ErrHelp = options.ErrHelp
+var ErrMissingTransform = options.ErrMissingTransform
+
+type AdditionalSettings = options.AdditionalSettings
+
+var ConfigureTransformer = options.ConfigureTransformer
+var ConfigureConnectors = options.ConfigureConnectors
+
+var GetRegisteredConnectors = options.GetRegisteredConnectors
